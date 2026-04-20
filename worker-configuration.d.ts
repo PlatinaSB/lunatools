82f300197e36e940fd55695b5db452de645f9a73
PlatinaSB <59110147+PlatinaSB@users.noreply.github.com>
2026-04-20 08:48:54 +0700

    fix formating

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -3,7 +3,7 @@
 // Runtime types generated with workerd@1.20260415.1 2026-04-19 nodejs_als
 declare namespace Cloudflare {
 	interface GlobalProps {
-		mainModule: typeof import("./.svelte-kit/cloudflare/_worker");
+		mainModule: typeof import('./.svelte-kit/cloudflare/_worker');
 	}
 	interface Env {
 		ASSETS: Fetcher;
